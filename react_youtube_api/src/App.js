1ed88852627d29bacd6ddf3afcf8d300f5f5db61
shridatta <sgz2@njit.edu>
2018-05-28 03:10:04 -0400

    added fully working components

--- react_youtube_api/src/App.js
+++ react_youtube_api/src/App.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/search_bar'
+import _ from 'lodash';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+const API_KEY = 'AIzaSyA-FRKIH2X4ByUdvLMch6dTqcpe-OjmvB0';
+
 
 class App extends Component {
-  render() {
+  constructor(props) {
+      
+    super(props);
+    
+    this.state = { 
+      videos : [] ,
+      selectedVideo:null
+    };
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key : API_KEY, term :term }, (videos) => {
+      this.setState({
+        
+        videos:videos,
+        selectedVideo:videos[0],
+        
+        });
+   });
+  }
+
+  render() { 
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+
     return (
       <div className="App">
-        <h1>HEllo World</h1>
-        <SearchBar />
+        <h1>Youtube Search API</h1>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
+  
 }
 
 export default App;
--- react_youtube_api/src/index.js
+++ react_youtube_api/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyA-FRKIH2X4ByUdvLMch6dTqcpe-OjmvB0';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
